0f7aad5b5879ffdeda0f010a0c5c1dcd4f75c27f
Matt Thornington <mattbonn@gmail.com>
2022-08-25 21:37:55 +0200

    grab4 works

---
+++ aoc-2021-5.py
@@ -0,0 +1,16 @@
+
+counter = 0
+codes = (0, 9, 5, 9, 8, 0, 0, 8, 9, 4, 3, 4, 2, 2, 2, 1, 7, 0, 7, 4,6, 4, 2, 0, 0, 9, 2, 9, 3, 4, 1, 4, 0, 0, 8, 8, 5, 5, 8, 2,)
+
+def grab4():
+    global counter
+    # counter = global counter
+    coords = (codes[counter], codes[counter+1], codes[counter+2], codes[counter+3])
+
+    print(coords)
+    counter = counter + 4
+
+while counter < len(codes):
+    print(counter)
+    grab4()
+# grab4()
