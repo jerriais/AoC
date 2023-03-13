b04997aeb557abbec50af5dc8dd67df2712ceb23
Matthew Thornington <jerriais@users.noreply.github.com>
2023-03-13 17:10:21 +0000

    silver prices

--- import time.py
+++ import time.py
@@ -1,22 +1,32 @@
+"""docstring"""
+
 import random
 import time
 
 startTime = time.time()
 
-prices = []
-for i in range(0,5000):
-  n = random.randint(1,1000000)
-  prices.append(n)
+prices = [4,3,2,7,8,9]
+# for i in range(0, 5000):
+#     n = random.randint(1, 1000000)
+#     prices.append(n)
 
-print (max(prices))
-print (min(prices))
-print (prices[0-10])
+buyday = 0
+sellday = 1
+
+profit = prices[sellday] - prices[buyday]
+
+for=>
+
+print(max(prices))
+print(min(prices))
+print(prices[0-5])
+print (profit)
 
 prevPrice = max(prices)
 
 for x in prices:
-  if (x > prevPrice):
-    buyPrice = prevPrice
+    if x > prevPrice:
+        buyPrice = prevPrice
 
 executionTime = (time.time() - startTime) * 1000
 print('Execution time in milliseconds: ' + "{:.2f}".format(executionTime))
