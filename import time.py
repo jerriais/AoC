f59e320603888296f8cb881b47ca0d7492286555
Matthew Thornington <matthew.thornington@se.bosch.com>
2023-03-14 16:47:55 +0100

    final

--- import time.py
+++ import time.py
@@ -5,39 +5,31 @@ import time
 
 random.seed(10)
 
-startTime = time.time()
-
 # prices = [4,3,2,7,8,9]
 prices = []
-for i in range(0, 10):
+for i in range(0, 5000):
     n = random.randint(1, 5000)
     prices.append(n)
+
+startTime = time.time()
 
 buyDay = 0
 bestBuyDay = 0
 sellDay = 1
 bestSellDay = 1
 
-def calculateProfit(sellDay, buyDay):
-    if sellDay > buyDay:
-        return prices[sellDay] - prices[buyDay]
-
-maxProfit = calculateProfit(sellDay,buyDay)
+maxProfit = -5000
 
 for buyDay in range(len(prices)-1):
     for sellDay in range(buyDay,len(prices)):
-        if buyDay < sellDay:
-            profit = calculateProfit(sellDay, buyDay)
-            print(buyDay,sellDay,profit)
+        # if buyDay < sellDay:
+            profit = prices[sellDay] - prices[buyDay]
+            # print(buyDay,sellDay,profit)
             if profit > maxProfit:
                 maxProfit = profit
                 bestBuyDay = buyDay
                 bestSellDay = sellDay
 
-
-print(max(prices))
-print(min(prices))
-print ((prices))
 print(bestBuyDay,bestSellDay,maxProfit)
 
 executionTime = (time.time() - startTime) * 1000
